89f284e5583ff36668c2e90f9f1f5b55c85be60b
Anaximandro <max54eu@hotmail.com>
2018-05-21 12:59:30 -0300

    Initial commit

---
+++ sobre.php
@@ -0,0 +1,20 @@
+<?php require 'header.php'?>
+    <section id="sob">
+        <div class="mainSobre">
+            <article>
+                <h1>Sobre Nós</h1>
+                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.
+                </p>
+                <p>Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.
+                </p>
+            </article>
+            <aside>
+                <div class="dark">
+                    <h3>O que fazemos?</h3>
+                    <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.
+                    </p>
+                </div>
+            </aside>
+        </div>
+    </section>
+<?php require 'footer.php'?>
